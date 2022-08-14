949c690fc9458c2c794c0e2542a62a09a189c845
MohammadJavadBagheri <bagherijavad25452545@gmail.com>
2022-08-14 22:03:14 +0430

    simple full page slider added

---
+++ css simple full page slider/script.js
@@ -0,0 +1,33 @@
+let back = document.querySelector('.back');
+let next = document.querySelector('.next');
+
+let imageBoxs = document.querySelectorAll('.slider-box');
+let imageActiveIndex = 2;
+
+back.addEventListener('click',()=>{
+    imageBoxs[imageActiveIndex].classList.remove('active');
+    decreaseImageActiveIndex();
+    imageBoxs[imageActiveIndex].classList.add('active');
+})
+
+next.addEventListener('click',()=>{
+    imageBoxs[imageActiveIndex].classList.remove('active');
+    increaseImageActiveIndex();
+    imageBoxs[imageActiveIndex].classList.add('active');
+})
+
+function decreaseImageActiveIndex(){
+    if(imageActiveIndex == 0){
+        imageActiveIndex = imageBoxs.length -1;
+        return;
+    }
+    imageActiveIndex -=1;
+}
+
+function increaseImageActiveIndex(){
+    if(imageActiveIndex == imageBoxs.length -1){
+        imageActiveIndex = 0;
+        return;
+    }
+    imageActiveIndex +=1;
+}
